cff26753de9c9f75ba67e6f20774f6571a0f7740
Everson-s8 <eversonx100@gmail.com>
2023-05-06 13:09:36 -0300

    Atualização

--- src/br/edu/ifs/Ed/lista/ListaDupEnc.java
+++ src/br/edu/ifs/Ed/lista/ListaDupEnc.java
@@ -1,5 +1,7 @@
 package br.edu.ifs.Ed.lista;
 
+import java.util.Comparator;
+
 public class ListaDupEnc<T extends Comparable<T>> extends Lista<T> {
     public int tamanho;
     public NoDup<T> primeiro;
@@ -31,8 +33,31 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T> {
 
 
     public void incluir(T elemento, int posicao) throws Exception {
-        throw new Exception("Não implementado");
+        if (tamanho == 0){
+            throw new Exception("Lista Vazia");
+        }
+        if (posicao > tamanho | posicao < 0){
+            throw new Exception("A posicao e maior que o tamanho da lista, nao existindo valor nessa posicao");
+        }
+        if (posicao == 0){
+            incluirInicio(elemento);
+            throw new Exception("O valor foi colocado na posicao 0");
+        }
+        if (posicao == tamanho-1){
+           incluir(elemento);
+        }
+        NoDup<T> Elemento = new NoDup<>(elemento);
+        NoDup aux = primeiro.proximo;
+        for (int i = 1; i < tamanho-1 ; i++) {
+            if (posicao == i){
+                aux.anterior.proximo = Elemento;
+                Elemento.anterior = aux.anterior;
+                aux.anterior = Elemento;
+                Elemento.proximo = aux;
 
+            }
+           aux = aux.proximo;
+        }
     }
 
 
@@ -73,8 +98,35 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T> {
 
 
     public void remover(int posicao) throws Exception {
-        throw new Exception("Não implementado");
+        if (tamanho == 0){
+            throw new Exception("Lista Vazia");
+        }
+        if (posicao > tamanho | posicao < 0){
+            throw new Exception("A posicao e maior que o tamanho da lista, nao existindo valor nessa posicao");
+        }
+        NoDup aux = primeiro.proximo;
 
+            if (posicao == 0){
+                primeiro = primeiro.proximo;
+                primeiro.anterior = null;
+                tamanho--;
+                throw new Exception("Valor no primeiro indice, removido com sucesso");
+            }
+            if (posicao == tamanho-1){
+                ultimo = ultimo.anterior;
+                ultimo.proximo = null;
+                tamanho--;
+                throw new Exception("Valor no ultimo indice, removido com sucesso");
+            }
+        for (int i = 1; i < tamanho-1 ; i++) {
+            if (posicao == i){
+                aux.proximo.anterior = aux.anterior;
+                aux.anterior.proximo = aux.proximo;
+                tamanho--;
+                throw new Exception("Valor no indice" + "  " + i + "  " + "removido com sucesso");
+            }
+            aux = aux.proximo;
+        }
     }
 
 
@@ -109,4 +161,6 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T> {
             aux = aux.proximo;
         }System.out.printf("Null");
     }
+
 }
+
